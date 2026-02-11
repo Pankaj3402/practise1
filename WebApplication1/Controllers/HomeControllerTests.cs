66015116e6a63bcc789b81f94619cb9c62b0fc5a
Pankaj3402 <pankaj342002@gmail.com>
2026-02-11 19:33:15 +0530

    changes

--- WebApplication1/Controllers/HomeControllerTests.cs
+++ WebApplication1/Controllers/HomeControllerTests.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
+using System.Diagnostics;
 using WebApplication1.Controllers;
 using WebApplication1.Models;
 
@@ -10,50 +12,59 @@ namespace WebApplication1.Tests.Controllers
     [TestFixture]
     public class HomeControllerTests
     {
-        private HomeController? _controller;
-        private Mock<ILogger<HomeController>>? _mockLogger;
+        private HomeController _controller;
+        private Mock<ILogger<HomeController>> _mockLogger;
 
         [SetUp]
         public void Setup()
         {
             _mockLogger = new Mock<ILogger<HomeController>>();
             _controller = new HomeController(_mockLogger.Object);
+
+            // ✅ Mock HttpContext
+            var httpContext = new DefaultHttpContext();
+            httpContext.TraceIdentifier = "test-request-id";
+
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            // ✅ Ensure Activity is available
+            Activity.Current = new Activity("TestActivity");
+            Activity.Current.Start();
         }
 
         [Test]
         public void Index_Returns_ViewResult()
         {
-            var result = _controller!.Index();
+            var result = _controller.Index();
             Assert.That(result, Is.InstanceOf<ViewResult>());
         }
 
         [Test]
         public void Privacy_Returns_ViewResult()
         {
-            var result = _controller!.Privacy();
+            var result = _controller.Privacy();
             Assert.That(result, Is.InstanceOf<ViewResult>());
         }
 
         [Test]
         public void Error_Returns_ViewResult_With_ErrorViewModel()
         {
-            var result = _controller!.Error();
+            var result = _controller.Error() as ViewResult;
 
-            Assert.That(result, Is.InstanceOf<ViewResult>());
-
-            var viewResult = result as ViewResult;
-            Assert.That(viewResult!.Model, Is.InstanceOf<ErrorViewModel>());
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result!.Model, Is.InstanceOf<ErrorViewModel>());
         }
 
         [Test]
         public void Error_Model_Should_Have_RequestId()
         {
-            var result = _controller!.Error() as ViewResult;
-            Assert.That(result, Is.Not.Null);
-
+            var result = _controller.Error() as ViewResult;
             var model = result!.Model as ErrorViewModel;
-            Assert.That(model, Is.Not.Null);
-            Assert.That(model!.RequestId, Is.Not.Empty);
+
+            Assert.That(model!.RequestId, Is.Not.Null);
         }
     }
 }
