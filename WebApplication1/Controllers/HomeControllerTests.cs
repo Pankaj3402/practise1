12063e987734ccbe29c3f211af7bbef6facefd0a
Pankaj3402 <pankaj342002@gmail.com>
2026-02-11 16:38:11 +0530

    test cases added

---
+++ WebApplication1/Controllers/HomeControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+        private Mock<ILogger<HomeController>> _mockLogger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockLogger = new Mock<ILogger<HomeController>>();
+            _controller = new HomeController(_mockLogger.Object);
+        }
+
+        [Test]
+        public void Index_Returns_ViewResult()
+        {
+            // Act
+            var result = _controller.Index();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+
+        [Test]
+        public void Privacy_Returns_ViewResult()
+        {
+            // Act
+            var result = _controller.Privacy();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+
+        [Test]
+        public void Error_Returns_ViewResult_With_ErrorViewModel()
+        {
+            // Act
+            var result = _controller.Error() as ViewResult;
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Model, Is.InstanceOf<ErrorViewModel>());
+        }
+
+        [Test]
+        public void Error_Model_Should_Have_RequestId()
+        {
+            // Act
+            var result = _controller.Error() as ViewResult;
+            var model = result.Model as ErrorViewModel;
+
+            // Assert
+            Assert.That(model.RequestId, Is.Not.Null);
+        }
+    }
+}
